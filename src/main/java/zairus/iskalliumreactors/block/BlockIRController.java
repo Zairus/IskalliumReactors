6a0ff0503c4615600173a9b7acc6b49d4f5108bc
wGEric <wgeric@gmail.com>
2018-04-14 21:01:13 -0600

    Send chat message with info about reactor when controller is right clicked on

--- src/main/java/zairus/iskalliumreactors/block/BlockIRController.java
+++ src/main/java/zairus/iskalliumreactors/block/BlockIRController.java
@@ -4,7 +4,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import zairus.iskalliumreactors.IskalliumReactors;
@@ -31,5 +38,42 @@ public class BlockIRController extends Block implements ITileEntityProvider, IBl
 	public void registerTileEntity()
 	{
 		GameRegistry.registerTileEntity(TileEntityIRController.class, "tileEntityIRController");
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn,
+									BlockPos pos,
+									IBlockState state,
+									EntityPlayer playerIn,
+									EnumHand hand,
+									EnumFacing side,
+									float hitX,
+									float hitY,
+									float hit)
+	{
+		TileEntity tileEntity = worldIn.getTileEntity(pos);
+
+		if (tileEntity instanceof TileEntityIRController)
+		{
+			if (worldIn.isRemote || hand != EnumHand.MAIN_HAND)
+				return true;
+
+			String message;
+			TileEntityIRController tileEntityIRController = (TileEntityIRController) tileEntity;
+
+			if (tileEntityIRController.getIsReactor())
+			{
+				message = I18n.format("info.iskalliumReactors.controller.generating", tileEntityIRController.getReactorYield());
+			}
+			else
+			{
+				message = I18n.format("info.iskalliumReactors.controller.invalid");
+			}
+
+			playerIn.sendMessage(new TextComponentString(message));
+			return true;
+		}
+
+		return false;
 	}
 }
